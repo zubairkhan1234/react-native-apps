4d7a4ad2287fda569e043ec9c975e9c09b5305d6
zubairkhan1234 <zubairsaleem1210@gmail.com>
2024-05-18 14:46:24 +0500

    added new react native app

---
+++ medicalAppoinment/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text } from 'react-native'
+import Home from './src/screens/Home'
+
+function App() {
+  return (
+    <Home />
+  )
+}
+
+export default App
---
+++ medicalAppoinment/src/screens/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+function Home() {
+  return (
+   <Text>
+    Home Screens
+   </Text>
+  )
+}
+
+export default Home
