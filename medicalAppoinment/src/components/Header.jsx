f31b7861bfca90695fe383d923f43f510d616090
zubairkhan1234 <zubairsaleem1210@gmail.com>
2024-05-30 09:30:50 +0500

    added stack navigation

---
+++ medicalAppoinment/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+function Header() {
+    return (
+        <View>
+            <View>
+                <Text>
+                    Hi Steven
+                </Text>
+
+                
+            </View>
+
+        </View>
+    )
+}
+
+export default Header
