11cf021db12175f818943faee7e913b1430a5daa
zubairkhan1234 <zubairsaleem1210@gmail.com>
2025-01-13 00:11:57 +0500

    configure a svg to use svg images

--- medicalAppoinment/metro.config.js
+++ medicalAppoinment/metro.config.js
@@ -1,11 +1,22 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer/react-native")
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"]
+  };
+
+  return wrapWithReanimatedMetroConfig(config);
+})();
--- medicalAppoinment/src/screens/Home.jsx
+++ medicalAppoinment/src/screens/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, View, TouchableOpacity, ImageBackground, SafeAreaView } from 'react-native';
-
+import Logo from '../assets/svg/logo.svg';
 function Home({ navigation }) {
   return (
     <SafeAreaView>
@@ -8,6 +8,9 @@ function Home({ navigation }) {
         <Text>
           hello hllto
         </Text>
+        <View>
+        <Logo  width={50} height={50}/>
+        </View>
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'red' }}>
           <Text style={{color:'red'}} >Home screen</Text>
           <TouchableOpacity onPress={() => navigation.navigate('Contact')}>
